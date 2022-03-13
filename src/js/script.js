0366d411d4e45b7a0575e3ce6f88cd8532e2e4e7
Francisco Chaves <francisco.jschaves@gmail.com>
2022-03-13 20:03:57 -0300

    feat: add button clean all

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,24 @@
+function setButtonCleanAllVisibility() {
+  const tasks = document.querySelector('#tasks');
+  const cleanAll = document.querySelector('#clean-all');
+
+  if (tasks.childNodes.length == 0) {
+    cleanAll.style.cssText = 'display: none;';
+  } else {
+    cleanAll.style.cssText = 'display: block; margin: 0 auto;';
+  }
+}
+
+setButtonCleanAllVisibility();
+
+const cleanAll = document.querySelector('#clean-all');
+
+cleanAll.addEventListener('click', (event) => {
+  const tasks = document.querySelector('#tasks');
+  tasks.innerHTML = '';
+  setButtonCleanAllVisibility();
+});
+
 const form = document.querySelector('form');
 
 form.addEventListener('submit', (event) => {
@@ -8,28 +29,30 @@ form.addEventListener('submit', (event) => {
 function createTask() {
   const tasks = document.querySelector('#tasks');
   const todo = document.querySelector('#todo');
-  const todoValue = todo.value;
 
-  if (validateTask(todoValue, tasks)) todo.value = '';
+  if (validateTask(todo.value, tasks)) {
+    todo.value = '';
+    setButtonCleanAllVisibility();
+  }
 }
 
-function validateTask(todoValue, tasks) {
-  if (!!todoValue) {
-    addTask(tasks, todoValue);
+function validateTask(todo, tasks) {
+  if (!!todo) {
+    addTask(tasks, todo);
     return true;
   }
   return false;
 }
 
-function addTask(tasks, todoValue) {
+function addTask(tasks, todo) {
   const idTask = Math.random() * Math.random();
   tasks.insertAdjacentHTML(
     'afterbegin',
     `<div>
-        <input type="checkbox" id="task${idTask}" class="task">
-        <label for="task${idTask}">${todoValue}</label>
-        <span class="clean-task">X</span>
-      </div>`
+    <input type="checkbox" id="task${idTask}" class="task">
+    <label for="task${idTask}">${todo}</label>
+    <span class="clean-task color-danger">X</span>
+    </div>`
   );
   addCleanTask();
 }
@@ -40,6 +63,7 @@ function addCleanTask() {
   cleanTasks.forEach((cleanTask) => {
     cleanTask.addEventListener('click', (event) => {
       event.target.parentElement.remove();
+      setButtonCleanAllVisibility();
     });
   });
 }
