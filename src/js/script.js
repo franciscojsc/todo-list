afd9cddb3a26b53662d8cc1be4a34a20512798a6
Francisco Chaves <francisco.jschaves@gmail.com>
2022-03-13 18:49:44 -0300

    feat: add clean task

--- src/js/script.js
+++ src/js/script.js
@@ -13,7 +13,19 @@ form.addEventListener('submit', (event) => {
       `<div>
         <input type="checkbox" id="task${idTask}" class="task">
         <label for="task${idTask}">${todo}</label>
+        <span class="clean-task">X</span>
       </div>`
     );
+    addCleanTask();
   }
 });
+
+function addCleanTask() {
+  const cleanTasks = document.querySelectorAll('.clean-task');
+
+  cleanTasks.forEach((cleanTask) => {
+    cleanTask.addEventListener('click', (event) => {
+      event.target.parentElement.remove();
+    });
+  });
+}
