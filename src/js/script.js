1061e6be14b5df7489c4eb411df49ef33fc3996d
Francisco Chaves <francisco.jschaves@gmail.com>
2022-03-13 19:03:50 -0300

    refactor: create functions in script js

--- src/js/script.js
+++ src/js/script.js
@@ -2,25 +2,37 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+  createTask();
+});
 
+function createTask() {
   const tasks = document.querySelector('#tasks');
   const todo = document.querySelector('#todo');
   const todoValue = todo.value;
 
+  if (validateTask(todoValue, tasks)) todo.value = '';
+}
+
+function validateTask(todoValue, tasks) {
   if (!!todoValue) {
-    const idTask = Math.random() * Math.random();
-    tasks.insertAdjacentHTML(
-      'afterbegin',
-      `<div>
+    addTask(tasks, todoValue);
+    return true;
+  }
+  return false;
+}
+
+function addTask(tasks, todoValue) {
+  const idTask = Math.random() * Math.random();
+  tasks.insertAdjacentHTML(
+    'afterbegin',
+    `<div>
         <input type="checkbox" id="task${idTask}" class="task">
         <label for="task${idTask}">${todoValue}</label>
         <span class="clean-task">X</span>
       </div>`
-    );
-    addCleanTask();
-    todo.value = '';
-  }
-});
+  );
+  addCleanTask();
+}
 
 function addCleanTask() {
   const cleanTasks = document.querySelectorAll('.clean-task');
